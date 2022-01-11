dad12fbdc5f7e0fa0d7bfdbff4fb6fa77347d584
rkr-dev <rinil.kunhiraman@protonmail.com>
2022-01-10 17:46:47 -0800

    Updated server
     - Added  Dotenv
     - Cors Middleware
     - Cookie Parser Middlware
     - Logger

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,33 @@
-import express from 'express';
+import dotenv from 'dotenv'
+dotenv.config()
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
 
-const app = express();
+const app = express()
 
-app.get('/api/ping', (req, res) => {
-  res.send('pong');
-});
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
-app.listen(5000, () => console.log('Server running'));
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Hello from API' })
+})
+
+app.get('/api/ping', (req:Request, res:Response) => {
+  res.send('pong')
+})
+
+const PORT = process.env.PORT || 5000
+
+const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+process.on('UnhandledRejection', (err, promise) => {
+  console.log(`Error occured ${err.message}`)
+  server.close(() => process.exit(1))
+})
