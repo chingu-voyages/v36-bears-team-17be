328d3401f58f186a987bf756983d3ae25477a535
rkr-dev <rinil.kunhiraman@protonmail.com>
2022-01-10 18:20:28 -0800

    Mongo Atlas Setup
     -  Mongoose Added
     -  Connection to MongoDB Setup

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,10 @@ import express, { Request, Response } from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import morgan from 'morgan'
+import {connectMongo} from '../src/config/db'
 
 const app = express()
+connectMongo()
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
@@ -25,9 +27,12 @@ app.get('/api/ping', (req:Request, res:Response) => {
 
 const PORT = process.env.PORT || 5000
 
-const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+const server = app.listen(PORT, () =>
+  console.log(`$$ ~ Server running on port ${PORT}`)
+)
 
 process.on('UnhandledRejection', (err, promise) => {
   console.log(`Error occured ${err.message}`)
   server.close(() => process.exit(1))
 })
+ 
