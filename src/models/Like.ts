a8d4a5ae19adc792ac46f4804acfb23743a272e4
howlsmoving <75964136+chronoshivt@users.noreply.github.com>
2022-01-24 13:12:13 -0600

    fixes

--- src/models/Like.ts
+++ src/models/Like.ts
@@ -1,32 +1,26 @@
-import mongoose, { Document, PopulatedDoc } from 'mongoose'
-import { IUser } from './User'
-import { IPost } from './Post'
+import mongoose, { Document, PopulatedDoc } from "mongoose";
+import { IUser } from "./User";
+import { IPost } from "./Post";
 
 export interface ILike extends Document {
-    user: PopulatedDoc<IUser>
-    post: PopulatedDoc<IPost>
-    like: Boolean
-    date: Date
-
+  user: PopulatedDoc<IUser>;
+  post: PopulatedDoc<IPost>;
+  date: Date;
 }
 
 const LikeSchema = new mongoose.Schema<ILike>({
-      like: {
-        type: Boolean,
-        required: true,
-    },
-      date: { type: Date, default: Date.now },
-      user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-      },
-      post: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Post',
-        required: true,
-      }
-})
-LikeSchema.index({ post:1, user: 1 }, { unique: true })
+  date: { type: Date, default: Date.now },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+    required: true,
+  },
+});
+LikeSchema.index({ post: 1, user: 1 }, { unique: true });
 
-export const Like = mongoose.model<ILike>('Like', LikeSchema)
+export const Like = mongoose.model<ILike>("Like", LikeSchema);
