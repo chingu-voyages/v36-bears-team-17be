1a5d91c1be7831b6a5b0054841fa15ec90e26655
rkr-dev <rinil.kunhiraman@protonmail.com>
2022-01-25 00:36:49 -0800

    User routes
     - api/auth/profile
     - api/auth/updateprofile
     - api/auth/updatepassword

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,46 +1,102 @@
 import { Request, Response, NextFunction } from 'express';
-import { asyncHandler } from '../middlewares/async'
-import ErrorResponse from '../utils/errorResponse'
-import { User } from '../models/User'
-import { sendTokenResponse } from '../utils/sendTokenResponse'
+import { asyncHandler } from '../middlewares/async';
+import ErrorResponse from '../utils/errorResponse';
+import { User } from '../models/User';
+import { RequestWithUser } from '../types/express';
+import { sendTokenResponse } from '../utils/sendTokenResponse';
 
 // @desc      Register a new User
 // @route     POST /api/v1/auth/register
 // @access    Public
 
-export const registerUser = asyncHandler(async (req: Request, res: Response, next:NextFunction) => {
-  const user = await User.create(req.body)
-  sendTokenResponse(user, 200, res)
-})
+export const registerUser = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const user = await User.create(req.body);
+        sendTokenResponse(user, 200, res);
+    }
+);
 
 // @desc      Login route for a User
 // @route     POST /api/v1/auth/login
 // @access    Public
 
-export const loginUser = asyncHandler(async (req: Request, res: Response, next:NextFunction) => { 
-    const { email, username, password } = req.body
-    if (!(email || username) || !password) {
-      return next(
-        new ErrorResponse(`email or username and a password is required`, 400)
-      )
-    }
-    let loginType
+export const loginUser = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const { email, username, password } = req.body;
+        if (!(email || username) || !password) {
+            return next(
+                new ErrorResponse(
+                    `email or username and a password is required`,
+                    400
+                )
+            );
+        }
+        let loginType;
 
-    if (email) {
-      loginType = { email: `${email}` }
-    } else {
-      loginType = { username: `${username}` }
-    }
-    const user = await User.findOne(loginType).select('+password')
-    if (!user) {
-      return next(new ErrorResponse(`Invalid Credentials`, 401))
-    }
+        if (email) {
+            loginType = { email: `${email}` };
+        } else {
+            loginType = { username: `${username}` };
+        }
+        const user = await User.findOne(loginType).select('+password');
+        if (!user) {
+            return next(new ErrorResponse(`Invalid Credentials`, 401));
+        }
 
-    const isMatch = await user.matchPassword(password)
+        const isMatch = await user.matchPassword(password);
 
-    if (!isMatch) {
-      return next(new ErrorResponse(`Invalid Credentials`, 401))
+        if (!isMatch) {
+            return next(new ErrorResponse(`Invalid Credentials`, 401));
+        }
+
+        sendTokenResponse(user, 200, res);
     }
+);
 
-    sendTokenResponse(user, 200, res)
-})
+// @desc      Get current loggedin user
+// @route     GET /api/auth/me
+// @access    Private
+
+export const getMe = asyncHandler(
+    async (req: RequestWithUser, res: Response, next: NextFunction) => {
+        const user = await User.findById(req.user.id);
+        res.status(200).json({ success: true, data: user });
+    }
+);
+
+// @desc      Update profile of the loggedin user
+// @route     PUT /api/auth/updateprofile
+// @access    Private
+
+export const updateProfile = asyncHandler(
+    async (req: RequestWithUser, res: Response, next: NextFunction) => {
+        const fields = {
+            name: req.body.name,
+            email: req.body.email,
+            ...req.body,
+        };
+        const user = await User.findByIdAndUpdate(req.user.id, fields, {
+            new: true,
+            runValidators: true,
+        });
+        res.status(200).json({ success: true, data: user });
+    }
+);
+
+// @desc      Update password of the loggedin user
+// @route     PUT /api/auth/updatepassword
+// @access    Private
+
+export const updatePassword = asyncHandler(
+    async (req: RequestWithUser, res: Response, next: NextFunction) => {
+        const user = await User.findById(req.user.id).select('+password');
+
+        // check current password
+        if (!user.matchPassword(req.body.currentPassword)) {
+            return next(new ErrorResponse(`Password is incorrect`, 401));
+        }
+        user.password = req.body.newPassword;
+        await user.save();
+        sendTokenResponse(user, 200, res);
+    }
+);
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,14 @@
 import express from 'express'
-import { registerUser, loginUser } from '../controllers/auth'
+import { registerUser, loginUser, getMe, updateProfile, updatePassword } from '../controllers/auth'
+import { protect } from '../middlewares/auth';
 
 const router:any = express.Router()
 
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
+
+router.route('/profile').get(protect, getMe);
+router.route('/updateprofile').put(protect, updateProfile);
+router.route('/updatepassword').put(protect, updatePassword);
 
 export default router
